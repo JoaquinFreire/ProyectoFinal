556093f567e3672df838ef0ded8f04f68fe3b2be
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2024-11-07 00:39:04 -0300

    Segundo Commit

--- ProyectoFinal/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ ProyectoFinal/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e747c86bb31fd9355309612b400ab0daae0e9c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
